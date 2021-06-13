e967cdfca297bab090503d41746214897201fee5
deje0014 <72152264+deje0014@users.noreply.github.com>
2021-06-13 14:26:38 -0400

    Updates

---
+++ precache-manifest.6a658faccd2685e43b0cbdec23b1ac09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b6c805298a3fb8ee844c846efc4cecf",
+    "url": "/clothing-store/index.html"
+  },
+  {
+    "revision": "6f66175c7fa42ceef69e",
+    "url": "/clothing-store/static/css/main.1048e159.chunk.css"
+  },
+  {
+    "revision": "d9573cdd0145d0ef91bd",
+    "url": "/clothing-store/static/js/2.8679f60a.chunk.js"
+  },
+  {
+    "revision": "902b3b97a6b7ea4b079c",
+    "url": "/clothing-store/static/js/3.ea7d9ed1.chunk.js"
+  },
+  {
+    "revision": "6f66175c7fa42ceef69e",
+    "url": "/clothing-store/static/js/main.6c6cd7df.chunk.js"
+  },
+  {
+    "revision": "f5e6f39c0a16f2153f31",
+    "url": "/clothing-store/static/js/runtime~main.45ebcf46.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/clothing-store/static/media/crown.ed28accb.svg"
+  }
+]);
